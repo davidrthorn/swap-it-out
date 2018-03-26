9361bc77ff37f93fcb9ba80027d94a626538ce02
David Rowthorn <davidrowthorn@gmail.com>
2018-03-26 16:33:30 +0100

    portions and nutdata from sql

---
+++ server/src/create-store.js
@@ -0,0 +1,13 @@
+// TODO
+// RDAS TO BE DONE CLIENTSIDE.
+// 1. Hone the SQL query
+// 2. Calculate portions
+// 3. Build object
+//    a. Convert to portions
+//    b. Split into target and swaps
+
+module.exports = {
+  createStore (portionData, nutData) {
+    return nutData
+  }
+}
--- server/src/queries.js
+++ server/src/queries.js
@@ -1,12 +1,20 @@
+require('dotenv').config()
+const swaps = require('./swaps')
 const promise = require('bluebird')
-
-const options = {
-  promiseLib: promise
-}
+const options = { promiseLib: promise }
+const createStore = require('./create-store')
 
 const pgp = require('pg-promise')(options)
-const connectionString = 'postgres://davidr:testapp@localhost:5432/usda'
-const db = pgp(connectionString)
+
+const cn = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+}
+
+const db = pgp(cn)
 
 module.exports = {
   getFoodByID: getFoodByID
@@ -14,30 +22,39 @@ module.exports = {
 
 function getFoodByID (req, res, next) {
   let foodID = req.params.id
-  db.any(`SELECT food_des.ndb_no AS food_id,
-    food_des.lONg_desc AS food_name,
-    nutr_def.nutrdesc AS nutrient_name,
-    nut_data.nutr_val AS nutrient_value,
-    nutr_def.units AS units
-    FROM food_des
-    INNER join nut_data ON food_des.ndb_no=nut_data.ndb_no
-    INNER join nutr_def ON nut_data.nutr_no=nutr_def.nutr_no
-    WHERE food_des.ndb_no = $1`,
-  `${foodID}`)
-    .then(data => {
-      res.status(200)
-        .json({
-          status: 'success',
-          message: `Retrieved record with ID ${foodID} from USDA database.`,
-          name: data[0].food_name,
-          data: (() => {
-            let nutr = {}
-            data.forEach(d => {
-              nutr[d.nutrient_name] = d.nutrient_value + d.units
+  db.any(`SELECT DISTINCT  ndb_no,
+     CASE
+       WHEN msre_desc LIKE '%medium%' AND gm_wgt BETWEEN 25 AND 150
+         THEN msre_desc
+       ELSE
+         'standard'
+     END AS pt_name,
+     CASE
+       WHEN msre_desc LIKE '%medium%' AND gm_wgt BETWEEN 25 AND 150
+         THEN gm_wgt
+      ELSE
+        100
+    END AS pt_wgt
+    FROM weight
+    WHERE ndb_no IN ($1:csv)`,
+  [swaps[foodID]])
+    .then(portionData => {
+      db.any(`SELECT food_des.ndb_no, food_des.long_desc, nutr_def.nutrdesc, nut_data.nutr_val
+        FROM food_des
+        INNER JOIN nut_data ON nut_data.ndb_no=food_des.ndb_no
+        INNER JOIN nutr_def ON nutr_def.nutr_no=nut_data.nutr_no
+        WHERE food_des.ndb_no IN ($1:csv)`,
+      [swaps[foodID]])
+        .then(nutData => {
+          let store = createStore.createStore(portionData, nutData)
+          res.status(200)
+            .json({
+              status: 'success',
+              message: `Retrieved record with ID ${foodID} from USDA database.`,
+              data: store
             })
-            return nutr
-          })()
         })
+        .catch(err => next(err))
     })
     .catch(err => next(err))
 }
